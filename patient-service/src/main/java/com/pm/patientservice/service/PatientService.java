00c9c9130b2c9be3218187bd0ab72c948c50001b
bobwen0205 <wenmingzhi10@gmail.com>
2025-11-07 03:24:00 -0500

    feature: url query finished

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,5 +1,6 @@
 package com.pm.patientservice.service;
 
+import com.pm.patientservice.dto.PagedPatientResponseDTO;
 import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
@@ -9,6 +10,10 @@ import com.pm.patientservice.kafka.KafkaProducer;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -27,9 +32,35 @@ public class PatientService {
         this.kafkaProducer = kafkaProducer;
     }
 
-    public List<PatientResponseDTO> getPatients() {
-        List<Patient> patients = patientRepository.findAll();
-        return patients.stream().map(PatientMapper::toDTO).toList();
+    public PagedPatientResponseDTO getPatients(
+            int page, int size, String sort, String sortField, String searchValue) {
+        // request -> page = 1
+        // pageable -> page = 0
+        Pageable pageable = PageRequest.of(page - 1, size,
+                sort.equalsIgnoreCase("desc")
+                        ? Sort.by(sortField).descending()
+                        : Sort.by(sortField).ascending());
+
+        Page<Patient> patientPage;
+
+        if (searchValue == null || searchValue.isBlank()) {
+            patientPage = patientRepository.findAll(pageable);
+        } else {
+            patientPage = patientRepository.findByNameContainingIgnoreCase(searchValue, pageable);
+        }
+
+        List<PatientResponseDTO> patientResponseDTOs = patientPage.getContent()
+                .stream()
+                .map(PatientMapper::toDTO)
+                .toList();
+
+        return new PagedPatientResponseDTO(
+                patientResponseDTOs,
+                patientPage.getNumber() + 1,
+                patientPage.getSize(),
+                patientPage.getTotalPages(),
+                (int) patientPage.getTotalElements()
+        );
     }
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
@@ -40,7 +71,7 @@ public class PatientService {
         Patient newPatient = patientRepository.save(PatientMapper.toEntity(patientRequestDTO));
 
         billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(), newPatient.getName(), newPatient.getEmail());
-        
+
         kafkaProducer.sendEvent(newPatient);
 
         return PatientMapper.toDTO(newPatient);
