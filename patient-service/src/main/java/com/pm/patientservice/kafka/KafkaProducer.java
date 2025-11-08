cbda065afbea30f6c286261171c7174ab8584d83
bobwen0205 <wenmingzhi10@gmail.com>
2025-11-08 18:57:47 -0500

    Feature: Circuit Breaker integrated

--- patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -1,5 +1,6 @@
 package com.pm.patientservice.kafka;
 
+import billing.events.BillingAccountEvent;
 import com.pm.patientservice.model.Patient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,6 +30,21 @@ public class KafkaProducer {
             kafkaTemplate.send("patient", event.toByteArray());
         } catch (Exception e) {
             log.error("Error sending PatientCreated event: {}", event);
+        }
+    }
+
+    public void sendBillingAccountEvent(String patientId, String name, String email) {
+        BillingAccountEvent event = BillingAccountEvent.newBuilder()
+                .setPatientId(patientId)
+                .setName(name)
+                .setEmail(email)
+                .setEventType("BILLING_ACCOUNT_CREATE_REQUESTED")
+                .build();
+
+        try {
+            kafkaTemplate.send("billing-account", event.toByteArray());
+        } catch (Exception e) {
+            log.error("Error sending BillingAccountCreated event: {}", e.getMessage());
         }
     }
 }
