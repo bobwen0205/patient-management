7c2ecb9d128870c531767317c6cc05457ae59670
bobwen0205 <wenmingzhi10@gmail.com>
2025-11-11 22:43:26 -0500

    CQRS and Appointment service finished

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -86,7 +86,7 @@ public class PatientService {
 
         billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(), newPatient.getName(), newPatient.getEmail());
 
-        kafkaProducer.sendEvent(newPatient);
+        kafkaProducer.sendPatientCreatedEvent(newPatient);
 
         return PatientMapper.toDTO(newPatient);
     }
@@ -104,6 +104,9 @@ public class PatientService {
         patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
 
         Patient updatedPatient = patientRepository.save(patient);
+
+        kafkaProducer.sendPatientUpdatedEvent(updatedPatient);
+
         return PatientMapper.toDTO(updatedPatient);
     }
 
